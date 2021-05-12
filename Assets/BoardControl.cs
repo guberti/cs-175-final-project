449a6af5b2ae8d33982f6c6c91f37758c3877286
guberti <guberti@DESKTOP-F7O4PP3>
2021-05-11 23:53:44 -0400

    Instantiate all pieces nicely

--- Assets/BoardControl.cs
+++ Assets/BoardControl.cs
@@ -3,134 +3,176 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public struct Square {
-	static float SQUARE_WIDTH = 1.5f;
+    static float SQUARE_WIDTH = 1.5f;
 
-	int rank; // 0-7 corresponds to 1-8
-	int file; // 0-7 corresponds to A-H
+    int rank; // 0-7 corresponds to 1-8
+    int file; // 0-7 corresponds to A-H
 
-	public Square(RaycastHit hit) {
-		rank = (int) (hit.point.x / SQUARE_WIDTH + 4);
-		file = (int) (hit.point.z / SQUARE_WIDTH + 4);
-		Debug.Log(rank);
-		Debug.Log(file);
-	}
+    public Square(RaycastHit hit) {
+        rank = (int) (hit.point.x / SQUARE_WIDTH + 4);
+        file = (int) (hit.point.z / SQUARE_WIDTH + 4);
+        Debug.Log(rank);
+        Debug.Log(file);
+    }
 
-	public Square(int rank, int file) {
-		this.rank = rank;
-		this.file = file;
-	}
+    public Square(int rank, int file) {
+        this.rank = rank;
+        this.file = file;
+    }
 
-	public Vector3 getCenter() {
-		return new Vector3((rank - 3.5f) * SQUARE_WIDTH, 0.001f, (file - 3.5f) * SQUARE_WIDTH);
-	}
+    public Vector3 getCenter() {
+        return new Vector3((rank - 3.5f) * SQUARE_WIDTH, 0.001f, (file - 3.5f) * SQUARE_WIDTH);
+    }
 }
 
 public struct PieceContainer {
-	static float MOVE_DURATION = 3f;
+    static float MOVE_DURATION = 3f;
 
-	GameObject model;
-	Square location;
+    GameObject model;
+    Square location;
 
-	public PieceContainer(GameObject model, Square location) {
-		this.model = model;
-		this.location = location;
-	}
+    public PieceContainer(GameObject model, Square location) {
+        this.model = model;
+        this.model.SetActive(true);
+        this.model.transform.position = location.getCenter();
+        this.location = location;
+    }
 
-	public void UpdateLocation(Square newLocation, MonoBehaviour monoBehavior) {
-		location = newLocation;
-		monoBehavior.StartCoroutine(SmoothMove());
-	}
+    public void UpdateLocation(Square newLocation, MonoBehaviour monoBehavior) {
+        location = newLocation;
+        monoBehavior.StartCoroutine(SmoothMove());
+    }
 
-	// Not a *real* s-curve, but a good simplification
-	private static float SCurve(float x) {
-		return 3f * Mathf.Pow(x, 2) - 2f * Mathf.Pow(x, 3);
-	}
+    // Not a *real* s-curve, but a good simplification
+    private static float SCurve(float x) {
+        return 3f * Mathf.Pow(x, 2) - 2f * Mathf.Pow(x, 3);
+    }
 
-	IEnumerator SmoothMove() {
-		float currentTime = 0;
-		Vector3 start = model.transform.position;
-		Vector3 end = location.getCenter();
+    IEnumerator SmoothMove() {
+        float currentTime = 0;
+        Vector3 start = model.transform.position;
+        Vector3 end = location.getCenter();
 
-		while(currentTime < MOVE_DURATION) {
-			model.transform.position = Vector3.Lerp(
-				start, end, SCurve(currentTime / MOVE_DURATION)
-			);
-			currentTime += Time.deltaTime;
-			yield return null;
-		}
-		model.transform.position = end;
-	}
+        while(currentTime < MOVE_DURATION) {
+            model.transform.position = Vector3.Lerp(
+                start, end, SCurve(currentTime / MOVE_DURATION)
+            );
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        model.transform.position = end;
+    }
 }
 
 public class BoardControl : MonoBehaviour
 {
 
-	public GameObject moveHighlightPrefab;
-	public List<PieceContainer> pieces;
+    // Chess piece prefabs
+    public GameObject whiteKing;
+    public GameObject whiteQueen;
+    public GameObject whiteRook;
+    public GameObject whiteBishop;
+    public GameObject whiteKnight;
+    public GameObject whitePawn;
 
-	private List<GameObject> tileHighlights;
+    public GameObject blackKing;
+    public GameObject blackQueen;
+    public GameObject blackRook;
+    public GameObject blackBishop;
+    public GameObject blackKnight;
+    public GameObject blackPawn;
 
-	private bool isSquareSelected;
-	private Square selectedSquare;
-	//private ChessGame gameManager;
+
+    public GameObject moveHighlightPrefab;
+    public List<PieceContainer> pieces;
+
+    private List<GameObject> tileHighlights;
+
+    private bool isSquareSelected;
+    private Square selectedSquare;
+    //private ChessGame gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-    	Debug.Log("Running start!");
-    	isSquareSelected = false;
-    	tileHighlights = new List<GameObject>();
-    	pieces = new List<PieceContainer>();
+        Debug.Log("Running start!");
+        this.gameObject.SetActiveRecursively(false);
+        this.gameObject.SetActive(true);
 
-    	// Instantiate GameObjects
-    	moveHighlightPrefab = GameObject.Find("SelectedSquare");
+        isSquareSelected = false;
+        tileHighlights = new List<GameObject>();
+        pieces = new List<PieceContainer>();
 
-    	pieces.Add(new PieceContainer(GameObject.Find("WhiteKing"), new Square(2, 2)));
-    	pieces[0].UpdateLocation(new Square(5, 5), this);
+        InstantiatePieces();
+    }
+
+    void InstantiatePieces() {
+        pieces.Add(new PieceContainer(Instantiate(whiteRook),   new Square(0, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteKnight), new Square(1, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteBishop), new Square(2, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteQueen),  new Square(3, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteKing),   new Square(4, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteBishop), new Square(5, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteKnight), new Square(6, 0)));
+        pieces.Add(new PieceContainer(Instantiate(whiteRook),   new Square(7, 0)));
+
+        pieces.Add(new PieceContainer(Instantiate(blackRook),   new Square(0, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackKnight), new Square(1, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackBishop), new Square(2, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackQueen),  new Square(3, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackKing),   new Square(4, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackBishop), new Square(5, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackKnight), new Square(6, 7)));
+        pieces.Add(new PieceContainer(Instantiate(blackRook),   new Square(7, 7)));
+
+        for (int i = 0; i < 8; i++) {
+            pieces.Add(new PieceContainer(Instantiate(whitePawn), new Square(i, 1)));
+            pieces.Add(new PieceContainer(Instantiate(blackPawn), new Square(i, 6)));
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-		CheckSelect();
+        CheckSelect();
 
     }
 
     private void CheckSelect()
     {
-    	if (Input.GetMouseButtonDown(0)) {
-    		RaycastHit hit;
-    		if (
-    			Physics.Raycast(
-    				Camera.main.ScreenPointToRay(Input.mousePosition), 
-    				out hit, 50f, LayerMask.GetMask("Board")
-    			)) {
-    			Debug.Log("Instantiating prefab!");
-    			Square clicked = new Square(hit);
-    			ProcessClickedSquare(clicked);
-    		}
-	    }
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            if (
+                Physics.Raycast(
+                    Camera.main.ScreenPointToRay(Input.mousePosition), 
+                    out hit, 50f, LayerMask.GetMask("Board")
+                )) {
+                Debug.Log("Instantiating prefab!");
+                Square clicked = new Square(hit);
+                ProcessClickedSquare(clicked);
+            }
+        }
     }
 
     private void ProcessClickedSquare(Square clicked) {
-    	if (!isSquareSelected) {
-    		selectedSquare = clicked;
-    		isSquareSelected = true;
-    		Square[] validMoves = {clicked};
+        if (!isSquareSelected) {
+            selectedSquare = clicked;
+            isSquareSelected = true;
+            Square[] validMoves = {clicked};
 
-    		foreach (Square validMove in validMoves) {
-				GameObject o = Instantiate(moveHighlightPrefab);
-				o.SetActive(true);
-				o.transform.position = clicked.getCenter();
-				tileHighlights.Add(o);
-    		}
-    	} else {
-    		isSquareSelected = false;
-    		foreach (GameObject highlight in tileHighlights) {
-				Destroy(highlight);
-    		}
-    	}
+            foreach (Square validMove in validMoves) {
+                GameObject o = Instantiate(moveHighlightPrefab);
+                o.SetActive(true);
+                o.transform.position = clicked.getCenter();
+                tileHighlights.Add(o);
+            }
+        } else {
+            isSquareSelected = false;
+            foreach (GameObject highlight in tileHighlights) {
+                Destroy(highlight);
+            }
+        }
     }
 
     private void RemoveHighlights() {
